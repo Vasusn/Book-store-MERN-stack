b10d74e11eb3cb176bc40abc0ba992a0d3e18dca
Vasu Nageshri <130688960+Vasusn@users.noreply.github.com>
2024-05-05 12:36:42 +0530

    Update CreateBooks.jsx

--- Frontend/src/pages/CreateBooks.jsx
+++ Frontend/src/pages/CreateBooks.jsx
@@ -21,7 +21,7 @@ const CreateBooks = () => {
     };
     setLoading(true);
     axios
-      .post('http://localhost:8888/books', data)
+      .post('https://book-store-mern-stackserver.vercel.app/books', data)
       .then(() => {
         setLoading(false);
         enqueueSnackbar('Book Created successfully', { variant: 'success' });
